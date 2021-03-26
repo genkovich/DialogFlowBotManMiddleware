3ba1e98fb6520794dcead0beb075193729c4f868
genkovich <k.sulimovsky@netpeak.net>
2021-03-26 17:23:18 +0200

    0.0.1 | initial commit

---
+++ src/Client.php
@@ -0,0 +1,137 @@
+<?php
+
+
+namespace BotMan\Middleware\DialogFlow\V2;
+
+use BotMan\BotMan\Messages\Incoming\IncomingMessage;
+use Google\ApiCore\ApiException;
+use Google\Cloud\Dialogflow\V2\DetectIntentResponse;
+use Google\Cloud\Dialogflow\V2\QueryInput;
+use Google\Cloud\Dialogflow\V2\QueryResult;
+use Google\Cloud\Dialogflow\V2\SessionsClient;
+use Google\Cloud\Dialogflow\V2\TextInput;
+
+class Client
+{
+    /**
+     * @var SessionsClient
+     */
+    private $sessionsClient;
+    /**
+     * @var string
+     */
+    private $languageCode;
+
+    /**
+     * Client constructor.
+     * @param string $languageCode
+     */
+    public function __construct(string $languageCode)
+    {
+        $this->languageCode = $languageCode;
+        $this->sessionsClient = new SessionsClient();
+    }
+
+
+    /**
+     * @param IncomingMessage $message
+     * @return Response
+     * @throws ApiException
+     */
+    public function getResponse(IncomingMessage $message): Response
+    {
+        $queryInput = $this->queryInput($message->getText(), $this->languageCode);
+        $intentResponse = $this->getIntentResponse(md5($message->getConversationIdentifier()), $queryInput);
+        $queryResult = $intentResponse->getQueryResult();
+
+        $response = new Response();
+        if (null === $queryResult || null === $queryResult->getIntent()) {
+            return $response;
+        }
+
+        $response->setIntent($queryResult->getIntent()->getDisplayName())
+            ->setParameters($this->getParameters($queryResult))
+            ->setContexts($this->getContexts($queryResult))
+            ->setAction($queryResult->getAction())
+            ->setReply($queryResult->getFulfillmentText())
+            ->setIsComplete(!$queryResult->getAllRequiredParamsPresent());
+
+        return $response;
+    }
+
+    /**
+     * @param $text
+     * @param string $languageCode
+     * @return QueryInput
+     */
+    private function queryInput($text, string $languageCode): QueryInput
+    {
+        $textInput = new TextInput();
+        $textInput->setText($text);
+        $textInput->setLanguageCode($languageCode);
+
+        $queryInput = new QueryInput();
+        $queryInput->setText($textInput);
+        return $queryInput;
+    }
+
+    /**
+     * @param $sessionId
+     * @param $queryInput
+     * @return DetectIntentResponse
+     * @throws ApiException
+     */
+    private function getIntentResponse($sessionId, $queryInput): DetectIntentResponse
+    {
+        $sessionName = $this->sessionsClient::sessionName(
+            getenv('GOOGLE_CLOUD_PROJECT'),
+            $sessionId ?: uniqid('', true)
+        );
+
+        $response = $this->sessionsClient->detectIntent($sessionName, $queryInput);
+        $this->sessionsClient->close();
+        return $response;
+    }
+
+    /**
+     * @param QueryResult $queryResult
+     * @return array
+     */
+    private function getParameters(QueryResult $queryResult): array
+    {
+        $parameters = [];
+        $queryParameters = $queryResult->getParameters();
+        if (null !== $queryParameters) {
+            foreach ($queryParameters->getFields() as $name => $field) {
+                $parameters[$name] = $field->getStringValue();
+            }
+        }
+
+        return $parameters;
+    }
+
+    /**
+     * @param QueryResult|null $queryResult
+     * @return array
+     */
+    private function getContexts(QueryResult $queryResult): array
+    {
+        $contexts = [];
+        foreach ($queryResult->getOutputContexts() as $context) {
+            $contextParams = [];
+            $parameters = $context->getParameters();
+            if (null !== $parameters) {
+                foreach ($parameters->getFields() as $name => $field) {
+                    $contextParams[$name] = $field->getStringValue();
+                }
+            }
+
+            $contexts[] = [
+                'name' => substr(strrchr($context->getName(), '/'), 1),
+                'parameters' => $contextParams,
+                'lifespan' => $context->getLifespanCount(),
+            ];
+        }
+        return $contexts;
+    }
+}
---
+++ src/DialogFlow.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace BotMan\Middleware\DialogFlow\V2;
+
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Interfaces\MiddlewareInterface;
+use BotMan\BotMan\Messages\Incoming\IncomingMessage;
+use Google\ApiCore\ApiException;
+
+class DialogFlow implements MiddlewareInterface
+{
+    /**
+     * @var bool
+     */
+    private $isIgnoreIntentPattern = false;
+
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * constructor.
+     * @param Client $client
+     */
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Create a new Dialogflow middleware instance.
+     * @param string $languageCode
+     *
+     * @return DialogFlow
+     */
+    public static function create(string $languageCode = 'en'): DialogFlow
+    {
+        $client = new Client($languageCode);
+        return new static($client);
+    }
+
+    /**
+     * Allow the middleware to listen all dialogflow actions.
+     *
+     * @return $this
+     */
+    public function ignoreIntentPattern(): DialogFlow
+    {
+        $this->isIgnoreIntentPattern = true;
+        return $this;
+    }
+
+    /**
+     * Handle a captured message.
+     *
+     * @param IncomingMessage $message
+     * @param BotMan $bot
+     * @param $next
+     *
+     * @return mixed
+     */
+    public function captured(IncomingMessage $message, $next, BotMan $bot)
+    {
+        return $next($message);
+    }
+
+    /**
+     * Handle an incoming message.
+     *
+     * @param IncomingMessage $message
+     * @param BotMan $bot
+     * @param $next
+     *
+     * @return mixed
+     * @throws ApiException
+     */
+    public function received(IncomingMessage $message, $next, BotMan $bot)
+    {
+
+        $response = $this->client->getResponse($message);
+        $message->addExtras('apiReply', $response->getReply() ?? '');
+        $message->addExtras('apiAction', $response->getAction() ?? '');
+        $message->addExtras('apiActionIncomplete', $response->isComplete() ?? false);
+        $message->addExtras('apiIntent', $response->getIntent() ?? '');
+        $message->addExtras('apiParameters', $response->getParameters() ?? []);
+        $message->addExtras('apiContexts', $response->getContexts() ?? []);
+
+        return $next($message);
+    }
+
+    /**
+     * @param IncomingMessage $message
+     * @param $pattern
+     * @param bool $regexMatched Indicator if the regular expression was matched too
+     *
+     * @return bool
+     */
+    public function matching(IncomingMessage $message, $pattern, $regexMatched): bool
+    {
+        if ($this->isIgnoreIntentPattern) {
+            return true;
+        }
+
+        $pattern = '/^' . $pattern . '$/i';
+        return (bool)preg_match($pattern, $message->getExtras()['apiAction']);
+    }
+
+    /**
+     * Handle a message that was successfully heard, but not processed yet.
+     *
+     * @param IncomingMessage $message
+     * @param BotMan $bot
+     * @param $next
+     *
+     * @return mixed
+     */
+    public function heard(IncomingMessage $message, $next, BotMan $bot)
+    {
+        return $next($message);
+    }
+
+    /**
+     * Handle an outgoing message payload before/after it
+     * hits the message service.
+     *
+     * @param mixed $payload
+     * @param BotMan $bot
+     * @param $next
+     *
+     * @return mixed
+     */
+    public function sending($payload, $next, BotMan $bot)
+    {
+        return $next($payload);
+    }
+}
